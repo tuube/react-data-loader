b6e4063878164a9c31207f346c2d9b495f0449a2
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-03-15 20:59:51 +0200

    Added example project

---
+++ example/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { DataLoaderProvider } from 'react-data-loader';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLoaderProvider>
+      <App />
+    </DataLoaderProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
