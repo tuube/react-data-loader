94835f669aeafe9348905dfc8f51aa663a752c46
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-03-15 21:30:11 +0200

    Build things

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,36 @@
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "./",
+      entryFileNames: pkg.main,
       format: "cjs",
     },
-    {
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({
+        declaration: true,
+        declarationDir: "dist/types/",
+        rootDir: "src/",
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
       file: pkg.module,
       format: "es",
     },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [typescript()],
-};
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [typescript()],
+  },
+];
