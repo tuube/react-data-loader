f655c13a192893f0b187dce581d6d708be4f66f9
Tuomas Inkinen <tuomas.inkinen@wunderdog.fi>
2021-03-17 20:01:34 +0200

    Fix

--- src/useDataLoader.ts
+++ src/useDataLoader.ts
@@ -1,24 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useEffect, useState } from 'react';
-import dataLoaderContext from './context';
-
+import { useContext, useEffect, useState } from "react";
+import dataLoaderContext from "./context";
 
 const useDataLoader = <T>(dataSource: string): T => {
   const dataLoader = useContext(dataLoaderContext);
   if (dataLoader === undefined) {
-    throw new Error('useDataLoaderContext must be used within DataLoaderProvider');
+    throw new Error(
+      "useDataLoaderContext must be used within DataLoaderProvider"
+    );
   }
 
   const [value, setValue] = useState<unknown>();
-  
+
   useEffect(() => {
     const sub = dataLoader.addSubscriber(dataSource, setValue);
     return () => {
-      dataLoader.removeSubscriber(sub);
-    }
+      if (sub !== undefined) dataLoader.removeSubscriber(sub);
+    };
   }, []);
 
-  return value as unknown as T;
-} 
+  return (value as unknown) as T;
+};
 
 export default useDataLoader;
